054cbd0902188316e4ec6a004951b36f08d2cbb0
Ciaran Jordan <ciaran@ciaranjordan.ie>
2022-03-09 18:53:59 +0000

    Add Room Membership Check

--- example.py
+++ example.py
@@ -4,10 +4,11 @@ from webex_bot.commands.echo import EchoCommand
 from webex_bot.webex_bot import WebexBot
 
 # Create a Bot Object
-bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"))
+bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"),
+               approved_rooms=["Y2lzY29zcGFyazovL3VzL1JPT00vMzZmZDU1YzAtOWZkNy0xMWVjLTkwZmQtYjU5MzAyZWRlOWQ1"])
 
 # Add new commands for the bot to listen out for.
-bot.add_command(EchoCommand())
+#bot.add_command(EchoCommand())
 
 # Call `run` for the bot to wait for incoming messages.
 bot.run()
