d0a29621c57a67fc3c8f524393889fbd349234bc
Finbarr Brady <fibrady@cisco.com>
2022-03-09 15:36:15 +0000

    Bump version: 0.2.12 → 0.2.13

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/webex_bot',
-    version='0.2.12',
+    version='0.2.13',
     zip_safe=False,
 )
