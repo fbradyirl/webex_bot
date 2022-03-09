6eb26d7663a9abbd84afae27594cfb9b8e55a938
Ciaran Jordan <ciaran@ciaranjordan.ie>
2022-03-09 19:07:33 +0000

    restore example.py

--- example.py
+++ example.py
@@ -4,11 +4,10 @@ from webex_bot.commands.echo import EchoCommand
 from webex_bot.webex_bot import WebexBot
 
 # Create a Bot Object
-bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"),
-               approved_rooms=["Y2lzY29zcGFyazovL3VzL1JPT00vMzZmZDU1YzAtOWZkNy0xMWVjLTkwZmQtYjU5MzAyZWRlOWQ1"])
+bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"))
 
 # Add new commands for the bot to listen out for.
-#bot.add_command(EchoCommand())
+bot.add_command(EchoCommand())
 
 # Call `run` for the bot to wait for incoming messages.
 bot.run()
