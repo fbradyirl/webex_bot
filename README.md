e5db5e8e9cadaaf3f565d819dfc549e7d53f8129
Finbarr Brady <fibrady@cisco.com>
2022-04-26 14:04:30 +0100

    Update readme.

--- README.md
+++ README.md
@@ -45,7 +45,9 @@ from webex_bot.webex_bot import WebexBot
 
 # Create a Bot Object
 bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"),
-               approved_rooms=['06586d8d-6aad-4201-9a69-0bf9eeb5766e'])
+               approved_rooms=['06586d8d-6aad-4201-9a69-0bf9eeb5766e'],
+               bot_name="My Teams Ops Bot",
+               include_demo_commands=True)
 
 # Add new commands for the bot to listen out for.
 bot.add_command(EchoCommand())
--- example.py
+++ example.py
@@ -4,7 +4,10 @@ from webex_bot.commands.echo import EchoCommand
 from webex_bot.webex_bot import WebexBot
 
 # Create a Bot Object
-bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"))
+bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"),
+               approved_rooms=['06586d8d-6aad-4201-9a69-0bf9eeb5766e'],
+               bot_name="My Teams Ops Bot",
+               include_demo_commands=True)
 
 # Add new commands for the bot to listen out for.
 bot.add_command(EchoCommand())
