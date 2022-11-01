8b83b099df7da5ffd22f1faa26e54054965db43f
Finbarr Brady <fbradyirl@users.noreply.github.com>
2022-11-01 09:35:55 +0000

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/webex_bot',
-    version='0.3.3',
+    version='0.3.4',
     zip_safe=False,
 )
