1a2709a91e224d4ed849ee94cfc12050e5d15dd3
Finbarr Brady <fbradyirl@users.noreply.github.com>
2024-04-24 11:19:41 +0100

    Fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/fbradyirl/webex_bot",
-    version="0.4.5",
+    version="0.4.4",
     zip_safe=False,
 )
