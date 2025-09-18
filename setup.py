f732aff5b0bddb3d34c5335d228f3e0b6a742fe4
Finbarr Brady <fibrady@cisco.com>
2025-09-18 10:36:01 +0100

    Bump version: 1.0.7 → 1.0.8

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/webex_bot',
-    version='1.0.7',
+    version='1.0.8',
     zip_safe=False,
 )
--- webex_bot/__init__.py
+++ webex_bot/__init__.py
@@ -1,4 +1,4 @@
 """Top-level package for Webex Bot."""
 
 __author__ = """Finbarr Brady"""
-__version__ = '1.0.7'
+__version__ = '1.0.8'
