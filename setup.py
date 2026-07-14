396c8df9870b8fe881125f451272594c12796151
Finbarr Brady <finbarr.brady@icloud.com>
2026-07-14 14:03:41 +0200

    Bump version: 1.2.8 → 1.3.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/webex_bot',
-    version='1.2.8',
+    version='1.3.0',
     zip_safe=False,
 )
